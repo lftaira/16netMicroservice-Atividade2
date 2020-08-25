ba96cb08859ab9ebd84439a210ed1586968e27f1
Luiz Taira <luiztaira@MacBook-Pro-de-Luiz-2.local>
2020-08-25 19:23:45 -0300

    primeiro commit

---
+++ Productor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Productor
+{
+    class Program
+    {
+
+        const string ConnectionString = "Endpoint=sb://sbtaira.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=0OZlOB5kPuyHQ9mV/B9IAihsGPreJRi8NdBEcCGhMN8=";
+        const string QueuePath = "productchanged";
+        static IQueueClient _queueClient;
+        static void Main(string[] args)
+        {
+            SendMessagesAsync().GetAwaiter().GetResult();
+            Console.WriteLine("messages were sent");
+            Console.ReadLine();
+
+        }
+
+        private static async Task SendMessagesAsync()
+        {
+            var queueClient = new QueueClient(QueueConnectionString,
+            QueuePath);
+            queueClient.OperationTimeout = TimeSpan.FromSeconds(10);
+            var messages = " Hi,Hello,Hey,How are you,Be Welcome"
+                .Split(',')
+                .Select(msg =>
+                {
+                    Console.WriteLine($"Will send message: {msg}");
+                    return new Message(Encoding.UTF8.GetBytes(msg));
+                })
+                .ToList();
+            var sendTask = queueClient.SendAsync(messages);
+            await sendTask;
+            CheckCommunicationExceptions(sendTask);
+            var closeTask = _queueClient.CloseAsync();
+            await closeTask;
+            CheckCommunicationExceptions(closeTask);
+
+        }
+        public bool CheckCommunicationExceptions(Task task)
+        {
+            if (task.Exception == null || task.Exception.InnerExceptions.Count == 0) return true;
+
+            task.Exception.InnerExceptions.ToList()
+                .ForEach(innerException =>
+            {
+                Console.WriteLine($"Error in SendAsync task:{ innerException.Message}.Details:{ innerException.StackTrace}");
+                if (innerException is ServiceBusCommunicationException)
+                    Console.WriteLine("Connection Problem with Host");
+            });
+            return false;
+        }
+    }
+}
---
+++ Reciever/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Reciever
+{
+    class Program
+    {
+        const string QueueConnectionString = "Endpoint=sb://sbtaira.servicebus.windows.net/;SharedAccessKeyName=ProductPolicy;SharedAccessKey=0OZlOB5kPuyHQ9mV/B9IAihsGPreJRi8NdBEcCGhMN8=";
+        const string QueuePath = "productschanged";
+        static IQueueClient _queueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static async Task ReceiveMessagesAsync()
+        {
+            _queueClient = new QueueClient(QueueConnectionString, QueuePath);
+            _queueClient.RegisterMessageHandler(MessageHandler,
+                new MessageHandlerOptions(ExceptionHandler) { AutoComplete = false });
+            Console.ReadLine();
+            await _queueClient.CloseAsync();
+        }
+
+        private static Task ExceptionHandler(ExceptionReceivedEventArgs exceptionArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionArgs.Exception}.");
+            var context = exceptionArgs.ExceptionReceivedContext;
+            Console.WriteLine($"Endpoint:{context.Endpoint}, Path:{context.EntityPath}, Action:{context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task MessageHandler(Message message, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Received message:{ Encoding.UTF8.GetString(message.Body)}");
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+    }
+}
